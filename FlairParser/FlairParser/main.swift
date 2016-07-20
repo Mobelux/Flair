231c1d335c4305a69f155e40ee6f67469fddb9d9
Andrew Mayers <andrew@mobelux.com>
2016-07-20 15:43:53 -0400

    Initial project setup

---
+++ FlairParser/FlairParser/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  FlairParser
+//
+//  Created by Jerry Mayers on 7/20/16.
+//  Copyright © 2016 Mobelux. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
---
+++ FlairParser/FlairParserFramework/FlairParserFramework.h
@@ -0,0 +1,19 @@
+//
+//  FlairParserFramework.h
+//  FlairParserFramework
+//
+//  Created by Jerry Mayers on 7/20/16.
+//  Copyright © 2016 Mobelux. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for FlairParserFramework.
+FOUNDATION_EXPORT double FlairParserFrameworkVersionNumber;
+
+//! Project version string for FlairParserFramework.
+FOUNDATION_EXPORT const unsigned char FlairParserFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlairParserFramework/PublicHeader.h>
+
+
