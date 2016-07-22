0326c088b5fc40d8b641b8771257b28c2c399fe3
Andrew Mayers <andrew@mobelux.com>
2016-07-22 15:19:20 -0400

    change the parser to be an Obj-C app so we can embed a Swift framework

---
+++ FlairParser/FlairParser/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FlairParser
+//
+//  Created by Jerry Mayers on 7/22/16.
+//  Copyright © 2016 Mobelux. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+@import FlairParserFramework;
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
