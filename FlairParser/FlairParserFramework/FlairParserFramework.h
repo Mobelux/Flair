6c514f90e8c03f7d243dc5f80636ce37504292f9
Andrew Mayers <andrew@mobelux.com>
2016-07-26 16:22:18 -0400

    compute hash of a string, and convert JSON to a string

--- FlairParser/FlairParserFramework/FlairParserFramework.h
+++ FlairParser/FlairParserFramework/FlairParserFramework.h
@@ -16,4 +16,4 @@ FOUNDATION_EXPORT const unsigned char FlairParserFrameworkVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <FlairParserFramework/PublicHeader.h>
 
-
+#import <FlairParserFramework/NSString+SHA256.h>
--- FlairParser/FlairParserFramework/Models/JSON.swift
+++ FlairParser/FlairParserFramework/Models/JSON.swift
@@ -8,4 +8,25 @@
 
 import Foundation
 
-public typealias JSON = [NSObject : AnyObject]
+public typealias JSONKey = NSObject
+public typealias JSONValue = AnyObject
+
+public typealias JSON = [JSONKey : JSONValue]
+
+enum JSONError: Int, ErrorProtocol {
+    case unknown = 10
+    case unparsableJSON = 11
+}
+
+extension Dictionary where Key: JSONKey, Value: JSONValue {
+    
+    func hash() throws -> String {
+        do {
+            let data = try JSONSerialization.data(withJSONObject: self, options: [.prettyPrinted])
+            guard let jsonString = String(data: data, encoding: .utf8), jsonString.characters.count > 0 else { throw JSONError.unparsableJSON }
+            return (jsonString as NSString).sha256()
+        } catch {
+            throw JSONError.unparsableJSON
+        }
+    }
+}
