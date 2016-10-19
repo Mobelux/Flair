66a074c82b6584b718ea48652f0686abe62488f6
Andrew Mayers <andrew@mobelux.com>
2016-10-19 14:45:40 -0400

    remove `s ` from prefix of style name
    `s ` is a way for the designers to define a static sized style, once we parse it we can remove it.

--- Sketch/Flair.sketchplugin/Contents/Sketch/textstyles.js
+++ Sketch/Flair.sketchplugin/Contents/Sketch/textstyles.js
@@ -30,6 +30,9 @@ flair.textStyles = {
     // Converts a MSSharedStyle into a Style JS object
     convertStyle: function (sharedStyle, workingScale, colorSets) {
     	var styleName = String(sharedStyle.name());
+        if (styleName.indexOf(flair.textStyles.staticStylePrefix) == 0) {
+            styleName = styleName.slice(flair.textStyles.staticStylePrefix.length);
+        }
     	var sanitizedName = flair.sanitizeName(styleName);
 
     	var isDynamicSize = true;
